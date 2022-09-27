06dae8cc0d1de2935fb3d1b96b006bc93fdb2c44
Andrea <andrearocca83@gmail.com>
2022-09-27 18:21:13 +0200

    added basic navigation and form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,15 +9,21 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
     recipes: Recipe[];
+    @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter();
+    
 
     constructor() { 
       this.recipes = [
           new Recipe('A Test Recipe','This is a test','https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg'),
-          new Recipe('A Test Recipe','This is a test','https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg')
+          new Recipe('Another Test Recipe','This is a test','https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg')
       ];
     }
 
     ngOnInit(): void {
+    }
+
+    onRecipeSelected(recipe: Recipe) {
+      this.recipeWasSelected.emit(recipe);
     }
 
 }
