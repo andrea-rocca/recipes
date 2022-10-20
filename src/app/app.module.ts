812da81d66e67a85e58d4b751785ce983087f1d3
Andrea <andrearocca83@gmail.com>
2022-10-20 18:58:47 +0200

    added core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { RecipesModule } from './recipes/recipes.module';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header/header.component';
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
 	declarations: [
@@ -25,24 +22,21 @@ import { SharedModule } from './shared/shared.module';
 	imports: [
 		BrowserModule,
 		FormsModule,
-		ReactiveFormsModule,
 		AppRoutingModule,
 		HttpClientModule,
 		RecipesModule,
 		ShoppingListModule,
-		SharedModule
+		SharedModule,
+		CoreModule
 	],
-	providers: [ShoppingListService, RecipeService, {
-		provide: HTTP_INTERCEPTORS,
-		useClass: AuthInterceptorService,
-		multi: true
-	}],
 	bootstrap: [AppComponent],
 
 	// needed for Angular versions before 9
 	// this is necessary for dynamically created components
+	/*
 	entryComponents: [
 		AlertComponent
 	]
+	*/
 })
 export class AppModule { }
