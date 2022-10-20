2ddbdff2d84e6af1b4e8bc6852dfbf553966c313
Andrea <andrearocca83@gmail.com>
2022-10-20 19:04:39 +0200

    auth module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RecipesModule } from './recipes/recipes.module';
 
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header/header.component';
 
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		AuthComponent,
 		HeaderComponent
 	],
 	imports: [
 		BrowserModule,
-		FormsModule,
 		AppRoutingModule,
 		HttpClientModule,
+		SharedModule,
+		CoreModule,
+		AuthModule,
 		RecipesModule,
 		ShoppingListModule,
-		SharedModule,
-		CoreModule
 	],
 	bootstrap: [AppComponent],
 
